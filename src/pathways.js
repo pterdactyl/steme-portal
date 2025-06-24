672364d919092fdef1381654e0b524287a274905
Silverstreak101 <94749697+Silverstreak101@users.noreply.github.com>
2025-06-24 10:45:21 -0400

    initial pathways files commited

---
+++ src/pathways.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './pathways.css'; //
+
+export default function Pathways() {
+
+  return (
+    <div>
+      <div className="navbar">
+        <a href="#home" className="nav-link">Engineering</a>
+        <a href="#about" className="nav-link">Life/Health Science</a>
+        <a href="#projects" className="nav-link">Computer Science</a>
+        <a href="#contact" className="nav-link">Business</a>
+      </div>
+    </div>
+
+    
+  ) 
+}
