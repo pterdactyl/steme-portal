2e0655299f58e48da58cdc03fded2c7b0c267b6b
Silverstreak101 <94749697+Silverstreak101@users.noreply.github.com>
2025-07-28 11:05:23 -0400

    pathways page height

--- src/pages/Student/pathways.js
+++ src/pages/Student/pathways.js
@@ -986,7 +986,7 @@ export default function Pathways() {
 
   return (
     <div style={{ paddingBottom: 30 }}>
-    <div className="pathways-container" style={{ position: "relative", height: 600}}>
+    <div className="pathways-container" style={{ position: "relative", minHeight: 600}}>
       <div className="navbar" style={{ marginBottom: 20 }}>
         {["engineering", "life-health-science", "computer-science", "business"].map(
           (cat) => (
